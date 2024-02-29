aee2f18f52465cf0bfdd418d079e07182ecd8cfa
pablo <pavelmeshkov2004@gmail.com>
2024-02-29 15:54:06 +0300

    progression fix

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,14 +3,14 @@ import { checkAnswer, getAnswerInt } from '../cli.js';
 const questionProgression = 'What number is missing in the progression?';
 
 const playRoundProgression = () => {
-  const start = Math.floor(Math.random() * 101);
-  const difference = Math.floor(Math.random() * 11) - 5;
+  const start = Math.floor(Math.random() * 8) + 1;
+  const difference = Math.floor(Math.random() * 5) + 2;
   const length = Math.floor(Math.random() * 6) + 5;
   const hiddenIndex = Math.floor(Math.random() * length);
 
   const progression = [];
   for (let i = 0; i < length; i += 1) {
-    progression.push(start + i * difference);
+    progression.push(start * Math.pow(difference, i));
   }
   const correctAnswer = progression[hiddenIndex];
 
